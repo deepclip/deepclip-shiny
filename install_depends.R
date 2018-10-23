116ff0f9af44f5f5144dd0d54c910023ad36f67e
Simon Larsen <simonhffh@gmail.com>
2018-10-23 13:34:02 -0700

    Added shinyWidgets to install_depends.R

--- install_depends.R
+++ install_depends.R
@@ -1,3 +1,1 @@
-# Should be provided in rocker/shiny: jsonlite, parallel
-#install.packages(c('shinyjs', 'DT', 'DBI', 'RSQLite', 'ggplot2', 'ggseqlogo', 'ggpubr', 'scales'))
-install.packages(c('shinyjs', 'DT', 'ggseqlogo', 'ggpubr'))
+install.packages(c('shinyjs', 'shinyWidgets', 'DT', 'ggseqlogo', 'ggpubr'))
