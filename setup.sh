230e98c276b69657885a5039f3f6fccf46fdeaff
Simon Larsen <simonhffh@gmail.com>
2019-02-01 20:32:07 +0100

    Force numpy 1.13.0

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pip2 install numpy==1.13.0
 pip2 install -r https://raw.githubusercontent.com/Lasagne/Lasagne/master/requirements.txt
 pip2 install https://github.com/Lasagne/Lasagne/archive/master.zip
 pip2 install matplotlib
