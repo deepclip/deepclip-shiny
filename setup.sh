096686b590613ac437ac5ac48f6fea1af8c7ee25
Simon Larsen <simonhffh@gmail.com>
2019-04-01 13:53:56 +0200

    Use newest version of numpy

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pip2 install numpy==1.13.0
 pip2 install -r https://raw.githubusercontent.com/Lasagne/Lasagne/master/requirements.txt
 pip2 install https://github.com/Lasagne/Lasagne/archive/master.zip
+pip2 install -U Theano
 pip2 install matplotlib
 pip2 install scikit-learn
 pip2 install biopython
